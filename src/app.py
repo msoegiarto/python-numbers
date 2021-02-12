0c2eb3392ac6ca69b2f1a34f29b4fbdf66fe929d
msoegiarto <soegiarto.mega@gmail.com>
2021-02-12 02:33:54 -0800

    Simplify iteration and string manipulation

--- src/app.py
+++ src/app.py
@@ -14,14 +14,10 @@ def print_numbers(number):
     if not number.isnumeric():
         return number + " is not numeric"
 
-    output = ""
-    i = 1
-
-    while i <= int(number):
-        output += (str(i) + " ")
-        i += 1
-
-    return output
+    # "{} ".format(i) = create a string with the value "i"
+    # bracket = create a list with values "1" to "n"
+    # create a new string consists of the values of the list joined with a space for each value.
+    return " ".join(["{}".format(i) for i in range(1, int(number) + 1)])
 
 
 @app.route("/<number>/odd")
@@ -29,13 +25,7 @@ def print_odd_numbers(number):
     if not number.isnumeric():
         return number + " is not numeric"
 
-    output = ""
-
-    for i in range(1, int(number) + 1):
-        if not is_even(i):
-            output += (str(i) + " ")
-
-    return output
+    return " ".join(["{}".format(i) for i in range(1, int(number) + 1, 2)])
 
 
 @app.route("/<number>/even")
@@ -43,13 +33,7 @@ def print_even_numbers(number):
     if not number.isnumeric():
         return number + " is not numeric"
 
-    output = ""
-
-    for i in range(1, int(number) + 1):
-        if is_even(i):
-            output += (str(i) + " ")
-
-    return output
+    return " ".join(["{}".format(i) for i in range(2, int(number) + 1, 2)])
 
 
 @app.route("/<number>/prime")
@@ -60,10 +44,8 @@ def print_prime_numbers(number):
     if int(number) <= 1:
         return "Prime number starts from 2"
 
-    output = ""
-
     number_range = int(number) + 1
-    prime = [True for i in range(number_range)]
+    prime = [True for _ in range(number_range)]  # _ because i is not needed
     prime[0] = prime[1] = False
 
     for i in range(2, number_range):
@@ -72,15 +54,11 @@ def print_prime_numbers(number):
             for j in range(i * i, number_range, i):
                 prime[j] = False
 
-    for i in range(2, number_range):
-        if prime[i]:
-            output += (str(i) + " ")
-
-    return output
-
-
-def is_even(number):
-    return number % 2 == 0
+    # output = ""
+    # for i in range(2, number_range):
+        # if prime[i]:
+            # output += "{}".format(i)
+    return " ".join(["{}".format(i) for i in range(2, number_range) if prime[i]])
 
 
 # run the flask app when running this file (app.py)
